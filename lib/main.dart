5aa097a3dd7c474637bec9ca962e154300a05e41
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-14 19:01:03 +0100

    termino da criacao da interface

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,58 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-          AppBar(title: Text("JokenPo"), backgroundColor: Colors.tealAccent),
-    );
+        appBar:
+            AppBar(title: Text("JokenPo"), backgroundColor: Colors.tealAccent),
+        body: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 32, bottom: 16),
+                child: Text(
+                  "Escolha do App",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Image.asset("images/padrao.png"),
+              Padding(
+                padding: EdgeInsets.only(top: 25, bottom: 30),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Center(
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(
+                        "images/pedra.png",
+                        height: 100,
+                      ),
+                      Image.asset(
+                        "images/papel.png",
+                        height: 100,
+                      ),
+                      Image.asset(
+                        "images/tesoura.png",
+                        height: 100,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
